b2d43d6d2382f5f86eeec3640d56bcf962677418
Juan Camilo <caamilo9517@gmail.com>
2023-02-15 16:55:12 -0500

    Lógica de compra de créditos parcialmente terminada

--- CompraCreditos/creditos.js
+++ CompraCreditos/creditos.js
@@ -3,12 +3,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      amountOfPickles:0,
+      //accumulatedPickles:0, //Estos son los pepinillos que el jugador tiene en total.
+      //amountOfPickles:0, //Papinillos que el usuario quiere comprar
+      //valuePickles:0
     }
   },
   methods:{
     getAmountOfPickles(e){
-      console.log(e.target)
+      //Esto se debería hacer una vez el usuario ya haya realizado el pago.
+      /*this.accumulatedPickles += e.target.dataset.amount
+      localStorage.setItem("accumulatedPickles",this.accumulatedPickles)*/
+      const salesData={
+        pickles:e.target.dataset.amount,
+        value:e.target.dataset.value
+      }
+      localStorage.setItem("salesData",JSON.stringify(salesData))
+
+      alert("Aquí se activa la redireción con la data de venta ya en el Storage")
+      
     }
   }
 }).mount('#app')
