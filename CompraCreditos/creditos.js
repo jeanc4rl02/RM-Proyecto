0d7998ddc7d9f13ef84dc501f344138f57d870cf
Juan Camilo <caamilo9517@gmail.com>
2023-02-15 21:55:33 -0500

    Implementación de ventanas modales para la pasarela de compras

--- CompraCreditos/creditos.js
+++ CompraCreditos/creditos.js
@@ -3,23 +3,23 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      //accumulatedPickles:0, //Estos son los pepinillos que el jugador tiene en total.
-      //amountOfPickles:0, //Papinillos que el usuario quiere comprar
-      //valuePickles:0
+      accumulatedPickles:0, //Pepinillos acumulados, debe venir de Storage.
+      amountOfPickles:0, //Papinillos que el usuario quiere comprar
+      valuePickles:0
     }
   },
   methods:{
     getAmountOfPickles(e){
-      //Esto se debería hacer una vez el usuario ya haya realizado el pago.
-      /*this.accumulatedPickles += e.target.dataset.amount
-      localStorage.setItem("accumulatedPickles",this.accumulatedPickles)*/
+      /*
+      this.accumulatedPickles += e.target.dataset.amount //Una vez finalizado el pago.
+      localStorage.setItem("accumulatedPickles",this.accumulatedPickles) */
+      this.amountOfPickles=e.target.dataset.amount
+      this.valuePickles=e.target.dataset.value
       const salesData={
-        pickles:e.target.dataset.amount,
-        value:e.target.dataset.value
+        pickles:this.amountOfPickles,
+        value:this.valuePickles
       }
-      localStorage.setItem("salesData",JSON.stringify(salesData))
 
-      alert("Aquí se activa la redireción con la data de venta ya en el Storage")
       
     }
   }
